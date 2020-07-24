825efd5f8ce220ed76376908e0e759bd05e9dd26
Jeregrande <jeregrande@yahoo.com>
2020-07-23 21:42:52 -0700

    Removed build from git ignore

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # CaptCheck
 A quiz app to test your nerd knowledge
+
+
+# To run:
+1. `git clone https://github.com/jeregrande/CaptCheck.git`
+2. `cd captcheck`
+3. `firebase serve`
---
+++ build/precache-manifest.df2cd0119a6636eb0083a77498521f26.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2b141e507fb9b43ce06ed07449a9ac9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b8d2e353aa002c4491d4",
+    "url": "/static/js/2.89330eb0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.89330eb0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d54d6937e9827bdd9d31",
+    "url": "/static/js/main.fad546b8.chunk.js"
+  },
+  {
+    "revision": "59418fcb114c4ac17f74",
+    "url": "/static/js/runtime-main.aba7fb27.js"
+  }
+]);
